01b05bdf7268ff0c58d19be64062c87e3ea85a5e
Zachary <zachness2003@gmail.com>
2021-11-16 08:11:53 -0600

    Cleaning up

--- api/app.js
+++ api/app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', loginRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {

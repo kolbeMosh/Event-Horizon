8142c6dcff97766ec7d9c90dd97587579e6ef555
Zachary <zachness2003@gmail.com>
2021-11-18 11:58:57 -0600

    Added dates for server and users

--- api/app.js
+++ api/app.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/user', userRouter);
 app.use('/message', messageRouter);
-app.use('/rooms', roomsRouter);
+app.use('/room', roomsRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     next(createError(404));
--- api/routes/room.js
+++ api/routes/room.js
@@ -29,11 +29,16 @@ router.get('/create/:name', (req, res) => {
     var roomIdQuery = 'select id from rooms';
     connection.query(roomIdQuery, (err, rows) => {
 
+        // Creates next usable id
         var lastID = parseInt(rows[rows.length - 1]['id'], 16);
         var nextID = (lastID + 1).toString(16);
 
+        // Gets current date
+        var date = new Date();
+        var dateString = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+
         // Adds new room to database
-        var addRoomQuery = 'insert into rooms(id, name, messageTable) values("' + nextID + '", "' + roomName + '", "' + roomName + nextID + 'Messages")';
+        var addRoomQuery = 'insert into rooms(id, name, messageTable, dateCreated) values("' + nextID + '", "' + roomName + '", "' + roomName + nextID + 'Messages", "' + dateString + '")';
         connection.query(addRoomQuery, (err2, rows2) => {
 
             if (err2) {
